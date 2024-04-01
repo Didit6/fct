16c6d1765e62853c53c3cdbb51a2b44e65dd6825
Didit6 <88450146+Didit6@users.noreply.github.com>
2024-04-02 06:43:56 +0700

    Add files via upload

---
+++ akfgcx/cyj0dcga45xftr.js
@@ -0,0 +1,10 @@
+var x9gs4 = `
+<footer class="footer mt-auto py-3 text-center">
+<div class="container">
+<a class="text-white" href="" target="_blank" rel="noopener">Twitter</a> |
+<a class="text-white" href="" target="_blank" rel="noopener">Telegram</a> |
+<a class="text-white" href="" target="_blank" rel="noopener">Email</a><br>
+<span>Copyright &copy; 2024 arial.eu.org</span>
+</div>
+</footer>`;
+document.body.insertAdjacentHTML("beforeend", x9gs4);
---
+++ akfgcx/gh564z0aNv.js
@@ -0,0 +1,37 @@
+var xhgs3 = `
+<nav class="navbar navbar-expand-lg navbar-dark mb-3">
+<div class="container">
+<a class="navbar-brand" href="index.html">arial.eu.org</a>
+<button class="navbar-toggler mb-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav me-auto">
+<li class="nav-item">
+<a class="nav-link rounded px-3" href="">Faucets</a>
+</li>
+<li class="nav-item">
+<a class="nav-link rounded px-3" href="">Faucetpay Faucets</a>
+</li>
+<li class="nav-item">
+<a class="nav-link rounded px-3" href="">Offers</a>
+</li>
+<li class="nav-item">
+<a class="nav-link rounded px-3" href="">More Crypto Websites</a>
+</li>
+</ul>
+<div class="d-flex ms-auto">
+<div class="form-check form-switch ms-auto" data-tippy-content="Toggle Day/Night Mode">
+<label class="form-check-label toggle-dn me-3" for="NightMode">
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 512 512">
+<path d="M494.9 331.3a20.6 20.6 0 0 0-23.2-4.2 214.2 214.2 0 0 1-119 17.6A215.5 215.5 0 0 1 187.5 40.3a20.6 20.6 0 0 0-27.3-27.4C116.5 32.3 29.9 94 13.7 210.8c-25.8 186.5 139 284.9 218.4 289.2 55.7 3.1 189.9 8.1 266.9-145.5 3.8-7.7 2-17.1-4.1-23.2zM237.7 459.2A215.5 215.5 0 0 1 131.8 78.7c-1.2 5.8-2.2 11.6-3 17.5-27.2 193.6 143 282.8 218.3 289.2a298 298 0 0 0 86-2.7 213 213 0 0 1-195.4 76.5z" />
+</svg>
+</label>
+<input class="form-check-input toggle-dn" type="checkbox" id="NightMode">
+</div>
+<div id="google_translate_element"></div>
+</div>
+</div>
+</div>
+</nav>`;
+document.body.insertAdjacentHTML("afterbegin", xhgs3);
